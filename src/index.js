be873a8d27e728616c9ee2819cc33a5952505dcf
Olya-Myalo <olyamyalo@mail.ru>
2023-10-31 20:39:05 +0300

    создан сервер

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 const http = require('http');
+const getUsers = require('./modules/users');
+const url = require('url');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true);
+    const queryObject = parsedUrl.query;
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+    if (parsedUrl.pathname === '/users') {
+        fs.readFile('data/users.json', (err, data) => {
+            if (err) {
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
+                response.end(getUsers());
+            } else {
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.end(data);
+            }
+        });
+    } else if ('hello' in queryObject) {
+        const name = queryObject['hello'];
+        if (name) {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end(`Hello, ${name}.`);
+        } else {
+            response.writeHead(400, { 'Content-Type': 'text/plain' });
+            response.end('Bad Request: Enter a name');
+        }
+    } else if (parsedUrl.pathname === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end('Hello, World!');
+    }
+});
 
+server.listen(port, hostname, () => {
+    console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
 });
